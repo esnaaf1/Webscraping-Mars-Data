3375aba27839eb082e839d8cf2a48492df714d3d
Farshad E <farshade@Farshads-MacBook-Pro.local>
2019-11-20 21:49:53 -0600

    Updates as of 20-NOV-2019 9:50 pm

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+mongo=PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+@app.route("/")
+def index():
+    mars_page = mongo.db.mars.find_one()
+    return render_template("index_html",mars=mars_page)
+
+@app.route("/scrape")
+def scrape():
+    mars_page = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert = True)
+    return "Sceaping Successful"
+
+if __name =="__main__":
+    app.run()
